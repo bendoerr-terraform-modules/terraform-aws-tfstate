e2a6edebd61aa8f8b85451640d6aef3cf85a19a2
Ben Doerr <craftsman@bendoerr.me>
2024-01-05 12:24:51 -0500

    🚨 (lint): Adds tflint config and addresses issues

--- examples/simple/ctx.tf
+++ examples/simple/ctx.tf
@@ -1,21 +1,3 @@
-terraform {
-  required_version = ">= 0.13"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-variable "namespace" {
-  type = string
-}
-
 module "context" {
   source      = "bendoerr-terraform-modules/context/null"
   version     = "0.4.1"
--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,24 @@
 output "bucket_id" {
-  value = module.store.s3_bucket_id
+  value       = module.store.s3_bucket_id
+  description = "The S3 bucket ID where the state will be stored."
 }
 
 output "bucket_arn" {
-  value = module.store.s3_bucket_arn
+  value       = module.store.s3_bucket_arn
+  description = "The S3 bucket ARN where the state will be stored."
 }
 
 output "lock_table_id" {
-  value = aws_dynamodb_table.locks.id
+  value       = aws_dynamodb_table.locks.id
+  description = "The DynamoDB table ID that will be used for distributed locking"
 }
 
 output "lock_table_arn" {
-  value = aws_dynamodb_table.locks.arn
+  value       = aws_dynamodb_table.locks.arn
+  description = "The DynamoDB table ARN that will be used for distributed locking"
 }
 
 output "lock_table_name" {
-  value = aws_dynamodb_table.locks.name
+  value       = aws_dynamodb_table.locks.name
+  description = "The DynamoDB table Name that will be used for distributed locking"
 }
